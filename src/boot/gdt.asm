80f9dd45b275286ee6e77c72ff0d84d1096d2666
George Doenlen <gdoenlen@gmail.com>
2018-10-23 14:12:10 -0400

    fix whitespace

--- src/boot/gdt.asm
+++ src/boot/gdt.asm
@@ -12,7 +12,7 @@ gdt_code:
     ; 1st flags: (present)1 (priviledge)00 (descriptor type)1 -> 1001b
     ; type flags: (code)1 (conforming)0 (readable)1 (accessed)0 -> 1010b
     ; 2nd flags: (granularity)1 (32-bit default)1 (64-bit seg)0 (AVL)0 -> 1100b
-    dw 0xffff     ; limit (bits 0 - 15)
+    dw 0xffff      ; limit (bits 0 - 15)
     dw 0x0         ; base (bits 0 - 15)
     db 0x0         ; base (bits 16 - 23)
     db 10011010b   ; 1st flags && type flags
@@ -23,7 +23,7 @@ gdt_code:
 gdt_data:
     ; same as the code segment except for the following type flags
     ; type flags: (code)0 (expand down)0 (writable)1 (accessed)0 -> 0010b
-    dw 0xffff     ; limit (bits 0 - 15)
+    dw 0xffff      ; limit (bits 0 - 15)
     dw 0x0         ; base (bits 0 - 15)
     db 0x0         ; base (bits 16 - 23)
     db 10010010b   ; 1st flags && type flags
