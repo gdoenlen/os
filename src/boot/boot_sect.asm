80e674fb943d3cec2967d5c69fd065c395e0e264
George Doenlen <gdoenlen@gmail.com>
2018-10-27 12:30:04 -0400

    finish make and extern kernel call

--- src/boot/boot_sect.asm
+++ src/boot/boot_sect.asm
@@ -27,7 +27,7 @@
         mov bx, MSG_LOAD_KERNEL
         call print_string
         mov bx, KERNEL_OFFSET
-        mov dh, 15
+        mov dh, 1
         mov dl, [BOOT_DRIVE]
         call disk_load
         ret
