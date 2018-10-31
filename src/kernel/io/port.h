4700db334df6be4dd4c48e62b3fd289ebf3829f9
George Doenlen <gdoenlen@gmail.com>
2018-10-30 23:32:26 -0400

    rename port io

---
+++ src/kernel/io/port.h
@@ -0,0 +1,9 @@
+#ifndef PORT_H
+#define PORT_H
+
+unsigned char port_byte_in(unsigned short port);
+void port_byte_out(unsigned short port, unsigned char data);
+unsigned char port_word_in(unsigned short port);
+void port_word_out(unsigned short port, unsigned short data);
+
+#endif
